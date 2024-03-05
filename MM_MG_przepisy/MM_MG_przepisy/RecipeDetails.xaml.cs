299e02992d881f471478a8b408f531ba43b3f168
Michał Molek <michal912456@gmail.com>
2024-03-05 12:28:02 +0100

    fill labels from code behind

--- MM_MG_przepisy/MM_MG_przepisy/RecipeDetails.xaml.cs
+++ MM_MG_przepisy/MM_MG_przepisy/RecipeDetails.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MM_MG_przepisy.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,14 @@ namespace MM_MG_przepisy
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RecipeDetails : ContentPage
     {
-        public RecipeDetails()
+        public RecipeDetails(Recipe recipe)
         {
             InitializeComponent();
+
+            Title = recipe.Name;
+            RecipeRate.Text = recipe.Rate + " / 5";
+            RecipeImage.Source = recipe.Image;
+            RecipeCookingTime.Text = recipe.CookingTime;
         }
     }
 }
