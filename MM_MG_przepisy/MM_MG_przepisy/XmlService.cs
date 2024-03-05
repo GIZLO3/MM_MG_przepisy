064885a0791526b9dbf672af5e427c8b65975173
Michał Giza <michal.giza@S50-05>
2024-03-05 12:06:45 +0100

    add static path and XmlSerializer type of ObservableCollection<Recipe>

--- MM_MG_przepisy/MM_MG_przepisy/XmlService.cs
+++ MM_MG_przepisy/MM_MG_przepisy/XmlService.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using MM_MG_przepisy.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace MM_MG_przepisy
 {
-    internal class XmlService
+    public class XmlService
     {
+        private static string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "recipes.xml");
+        private static XmlSerializer XmlSerializer = new XmlSerializer(typeof(ObservableCollection<Recipe>));
     }
 }
