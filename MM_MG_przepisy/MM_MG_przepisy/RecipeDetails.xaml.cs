a4600675777080afe9f43401674283557c198ac6
Michał Molek <michal912456@gmail.com>
2024-03-05 12:22:33 +0100

    add recipe detail page

---
+++ MM_MG_przepisy/MM_MG_przepisy/RecipeDetails.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MM_MG_przepisy
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RecipeDetails : ContentPage
+    {
+        public RecipeDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
